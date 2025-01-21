b092a0d091eaf09cdfce563579355e35818918ae
Joao Pires <mail@joaopires.com>
2025-01-21 08:36:51 +0000

    refactor to handle changes to SimpleNetwork

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/nmdias/FeedKit.git", exact: "9.1.2"),
         .package(url: "https://github.com/brightdigit/SyndiKit.git", exact: "0.3.7"),
-        .package(url: "https://github.com/joaoc-pires/lib-simple-network", exact: "1.1.2"),
+        .package(url: "https://github.com/joaoc-pires/lib-simple-network", exact: "1.2.0"),
         .package(url: "https://github.com/scinfu/SwiftSoup", exact: "2.6.1"),
     ],
     targets: [
--- Sources/AresCore/AresCore.swift
+++ Sources/AresCore/AresCore.swift
@@ -12,26 +12,7 @@ final public class AresCore {
         self.settings = defaultSettings ?? UserDefaults.standard
         self.log = Logger(subsystem: "AresCore", category: "AresCoreService")
     }
-    
-    /// Starts the synchronous download of multiple feeds. The result will be published using NotificationCenter. Any object that wants to receive the **successful** results of these operations must listen to Notification.Name.feedFinishedDownload
-    /// - Parameter feeds: A list of ARSCFeed
-    public func fetch(_ feeds: [ARSCFeed], ignoreEtag: Bool = false) {
-        fetch(feeds.map({ $0.id }), ignoreEtag: ignoreEtag)
-    }
-    
-    /// Starts the synchronous download of multiple feeds. The result will be published using NotificationCenter. Any object that wants to receive the **successful** results of these operations must listen to Notification.Name.feedFinishedDownload
-    /// - Parameter feeds: A list of URL strings
-    public func fetch(_ feeds: [String], ignoreEtag: Bool = false) {
-        for feed in feeds {
-            fetch(feed, ignoreEtag: ignoreEtag) { result in
-                switch result {
-                    case .success(let newFeed): NotificationCenter.default.post(name: .feedFinishedDownload, object: newFeed)
-                    case .failure(let error): self.log.error("failed to synchronously download '\(feed)' with error '\(error)', '\(error.localizedDescription)'")
-                }
-            }
-        }
-    }
-    
+        
     /// Starts a synchronous download of a given feed
     /// - Parameters:
     ///   - feedURL: the url of the feed to download
@@ -67,14 +48,29 @@ final public class AresCore {
     /// Starts an asynchronous download of a given feed URL
     /// - Parameter feedURL: the url of the feed to download
     /// - Returns: ARSCFeed object representing that feed, or throws any error in might have encountered when downloading and parsing
-    public func fetch(_ feedURL: String, ignoreEtag: Bool = false) async throws -> ARSCFeed {
-        try await withCheckedThrowingContinuation { continuation in
-            fetch(feedURL, ignoreEtag: ignoreEtag) { result in
-                switch result {
-                    case .success(let feed): continuation.resume(returning: feed)
-                    case .failure(let error): continuation.resume(throwing: error)
+    public func fetch(_ feedURL: String, ignoreEtag: Bool = false) async throws(AresCoreError) -> ARSCFeed {
+        let request = Request(url: feedURL, settings: self.settings)
+        let networkService = NetworkService()
+        let data: Data
+        do {
+            data = try await networkService.fire(request: request, ignoreEtag: ignoreEtag)
+        }
+        catch {
+            throw .networkError(error)
+        }
+        guard !data.isEmpty else { throw .cachedReply }
+        let parsedResult = FeedParser(data: data).parse()
+        switch parsedResult {
+            case .success(let feed):
+                self.log.info("parsed '\(data.count) bytes' from '\(feedURL)'")
+                switch feed {
+                    case let .atom(atomFeed):   return atomFeed.aresFeed(withID: feedURL)
+                    case let .rss(rssFeed):     return rssFeed.aresFeed(withID: feedURL)
+                    case let .json(jsonFeed):   return jsonFeed.aresFeed(withID: feedURL)
                 }
-            }
+            case .failure(let error):
+                self.log.error("failed to parse '\(data.count) bytes' from '\(feedURL)'")
+                throw .parsingError(error)
         }
     }
 
@@ -95,7 +91,7 @@ final public class AresCore {
     public struct Request: NetworkRequest, Hashable {
         
         public var url: String
-        public var eTag: String? { UserDefaults.standard.string(forKey: kEtag) }
+        public var eTag: String? { settings.string(forKey: kEtag) }
         public var method: SimpleNetwork.HTTPMethod { .get }
         public var sessionDelegate: (URLSessionTaskDelegate)?
         
@@ -108,13 +104,6 @@ final public class AresCore {
             self.url = url
             self.sessionDelegate = sessionDelegate
             self.settings = settings
-        }
-
-        public func getETagDataIfAvailable(_ response: HTTPURLResponse, _ data: Data) -> Data? {
-            if response.allHeaderFields.keys.contains("Etag"), let etagValue = response.allHeaderFields["Etag"] as? String {
-                UserDefaults.standard.setValue(etagValue, forKey: kEtag)
-            }
-            return nil
         }
         
         public func hash(into hasher: inout Hasher) {
